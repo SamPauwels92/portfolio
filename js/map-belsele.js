6a4f823909be228372252f5244f05652b72522de
SamPauwels92 <sam.pauwels.92@gmail.com>
2025-05-12 11:21:21 +0200

    update

--- js/map-belsele.js
+++ js/map-belsele.js
@@ -7,8 +7,16 @@ L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-// Place a marker on the location of the Church of Belsele
-let apMarker = L.marker([51.1700, 4.1375]).addTo(map);
+// Create a custom icon for the marker
+let spIcon = L.icon({
+    iconUrl: 'assets/images/SP-Logo.svg',  // Path to your custom SP logo (SVG)
+    iconSize: [32, 32],       // Size of the icon (adjust as needed)
+    iconAnchor: [16, 32],     // Point of the icon that will correspond to the marker's location
+    popupAnchor: [0, -32]     // Popup will appear above the icon
+});
+
+// Place a marker with the custom SP logo at the location of the Church of Belsele
+let apMarker = L.marker([51.1700, 4.1375], { icon: spIcon }).addTo(map);
 
 // Add a popup to the marker
 apMarker.bindPopup("<b>Church of Belsele</b><br>Location in Sint-Niklaas, Belgium").openPopup();
