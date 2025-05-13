111d56b263c9a2be64a8e6ab565b43b2e81f6387
SamPauwels92 <sam.pauwels.92@gmail.com>
2025-05-13 09:15:32 +0200

    update

--- js/map-belsele.js
+++ js/map-belsele.js
@@ -1,22 +1,22 @@
-// Initialize the map
-let map = L.map('map-belsele').setView([51.1700, 4.1375], 16); // Coordinates for the Church of Belsele and zoom level 16
+// Initialiseer de kaart
+let map = L.map('map-belsele').setView([51.1700, 4.1375], 16); // Coördinaten voor de Kerk van Belsele en zoomniveau 16
 
-// Add a dark tile layer (CartoDB Dark Matter in this case)
+// Voeg een donkere tegel-laag toe (CartoDB Dark Matter in dit geval)
 L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png', {
-    maxZoom: 19, // Maximum zoom level
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    maxZoom: 19, // Maximale zoomniveau
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> bijdragers'
 }).addTo(map);
 
-// Create a custom icon for the marker
+// Maak een aangepast icoon voor de marker
 let spIcon = L.icon({
-    iconUrl: 'assets/images/SP-Logo.svg',  // Path to your custom SP logo (SVG)
-    iconSize: [32, 32],       // Size of the icon (adjust as needed)
-    iconAnchor: [16, 32],     // Point of the icon that will correspond to the marker's location
-    popupAnchor: [0, -32]     // Popup will appear above the icon
+    iconUrl: 'assets/images/SP-Logo.svg',  // Pad naar je aangepast SP-logo (SVG)
+    iconSize: [32, 32],       // Grootte van het icoon (pas aan indien nodig)
+    iconAnchor: [16, 32],     // Punt van het icoon dat overeenkomt met de locatie van de marker
+    popupAnchor: [0, -32]     // De popup verschijnt boven het icoon
 });
 
-// Place a marker with the custom SP logo at the location of the Church of Belsele
+// Plaats een marker met het aangepaste SP-logo op de locatie van de Kerk van Belsele
 let apMarker = L.marker([51.1700, 4.1375], { icon: spIcon }).addTo(map);
 
-// Add a popup to the marker
-apMarker.bindPopup("<b>Church of Belsele</b><br>Location in Sint-Niklaas, Belgium").openPopup();
+// Voeg een popup toe aan de marker
+apMarker.bindPopup("<b>Kerk van Belsele</b><br>Locatie in Sint-Niklaas, België").openPopup();
